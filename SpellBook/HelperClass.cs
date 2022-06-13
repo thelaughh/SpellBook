c5d14435b35e63fb0d9ed51f3d6f1eba3b18e9a0
Jan Gergel <jan.gergel@educhem.cz>
2022-06-13 09:15:12 +0200

    Přidejte soubory projektu.

---
+++ SpellBook/HelperClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellBook
+{
+    internal class HelperClass
+    {
+        public Random generator = new Random();
+        public string[] PoleZTextu(string adresaSouboru)
+        {
+            string obsahTxt = File.ReadAllText(adresaSouboru);
+            return obsahTxt.Split(';');
+        }
+        public string VygenerujSpellname(string[] poleForma, string[] poleTypy)
+        {
+
+        }
+        public int VYgenerujCislo(int max)
+        {
+            
+        }
+       
+    }
+}
---
+++ SpellBook/Spell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellBook
+{
+    internal class Spell
+    {
+        public string Name { get; set; }
+        public int ManaCost { get; set; }
+
+        public void třída()
+        {
+            Console.WriteLine("Projekt vytvořen");
+        }
+    }
+}
